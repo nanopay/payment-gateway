ddb78f7485518795d0c15edca622488bc99234bb
anarkrypto <anarkrypto@gmail.com>
2023-03-19 19:31:07 -0300

    🏷️ Add Pusher envs to types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,15 @@
-export interface Environment {
+export interface Queues {
 	PAYMENT_LISTENER_QUEUE: Queue;
 	PAYMENT_WRITE_QUEUE: Queue;
+}
+export interface Environment extends Queues {
 	AUTH_TOKEN: string;
 	SUPABASE_URL: string;
 	SUPABASE_KEY: string;
 	NANO_WEBSOCKET_URL: string;
+	PUSHER_APP_ID: string;
+	PUSHER_KEY: string;
+	PUSHER_SECRET: string;
 }
 
 export interface Payment {
