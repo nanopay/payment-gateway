23fe486742d73d5bcf33fab86072adc3e51cdb74
anarkrypto <anarkrypto@gmail.com>
2023-05-08 12:51:03 -0300

    🚧 Add kvStore to wallet and improve code

--- src/nano/wallet.ts
+++ src/nano/wallet.ts
@@ -1,27 +1,41 @@
-import { checkAddress, createBlock, derivePublicKey, validateWork } from "nanocurrency";
+import { createBlock, deriveAddress, derivePublicKey, validateWork } from "nanocurrency";
 import NanoRPC, { NanoRPCProps } from "./rpc";
 import { RECEIVE_DIFFICULTY, SEND_DIFFICULTY } from "./constants";
+import { TunedBigNumber } from "../utils";
 
-interface NanoWalletProps extends NanoRPCProps {
+export interface NanoWalletProps extends NanoRPCProps {
     privateKey: string;
     representative: string;
+    kvStore: KVNamespace;
 }
 
-interface ReceiveData {
+export interface NanoWalletState {
+    balance: string;
+    frontier: string | null;
+}
+
+export interface ReceiveData {
     link: string;
     amount: string;
     previous?: string | null;
 }
 
-interface SendData {
+export interface SendData {
     link: string;
     previous: string;
 }
 
 export default class NanoWallet {
     rpc: NanoRPC;
-    privateKey: string;
+    private privateKey: string;
+    private kvStore: KVNamespace;
+    private publicKey: string;
+    account: string;
     representative: string;
+    state: NanoWalletState = {
+        balance: '0',
+        frontier: null,
+    };
 
     constructor({
         privateKey,
@@ -29,19 +43,55 @@ export default class NanoWallet {
         workerURLs,
         timeout = 30000,
         representative,
+        kvStore
     }: NanoWalletProps) {
         this.privateKey = privateKey;
+        this.publicKey = derivePublicKey(this.privateKey);
+        this.account = deriveAddress(this.publicKey);
+        this.kvStore = kvStore;
         this.rpc = new NanoRPC({ rpcURLs, workerURLs, timeout });
         this.representative = representative;
-        if (!checkAddress(this.representative)) {
-            throw new Error(`Invalid representative address: ${representative}`);
+    }
+
+    async init () {
+        const state = await this.kvStore.get(this.account);
+        if (state) {
+            this.state = JSON.parse(state);
         }
+    }
+
+    async update (state: Partial<NanoWalletState>) {
+        this.state = {
+            ...this.state,
+            ...state
+        };
+        this.kvStore.put(this.account, JSON.stringify(this.state));
+    }
+
+    async workGenerate (hash: string, threshold: string) {
+        const { work } = await this.rpc.workGenerate(hash, threshold);
+
+        if (!work) {
+            throw new Error('No work');
+        }
+
+        const isValidWork = validateWork({
+            work,
+            blockHash: hash,
+            threshold
+        });
+
+        if (!isValidWork) {
+            throw new Error('Invalid work');
+        }
+
+        return work;
     }
 
     async receive(data: ReceiveData) {
 
         const previous = data.previous || null
-        const balance = data.amount
+        const balance = TunedBigNumber(this.state.balance).plus(data.amount).toString();
 
         const { block, hash } = createBlock(this.privateKey, {
             previous,
@@ -51,23 +101,9 @@ export default class NanoWallet {
             work: null
         })
 
-        const frontier = previous || derivePublicKey(this.privateKey);
+        const frontier = previous || this.publicKey;
 
-        const { work } = await this.rpc.workGenerate(frontier, RECEIVE_DIFFICULTY);
-
-        if (!work) {
-            throw new Error('No work');
-        }
-
-        const isValidWork = validateWork({
-            work,
-            blockHash: frontier,
-            threshold: RECEIVE_DIFFICULTY
-        });
-
-        if (!isValidWork) {
-            throw new Error('Invalid work');
-        }
+        const work = await this.workGenerate(frontier, RECEIVE_DIFFICULTY);
 
         const processed = await this.rpc.process({
             ...block,
@@ -77,6 +113,11 @@ export default class NanoWallet {
         if (processed.hash !== hash) {
             throw new Error('Block hash mismatch');
         }
+
+        await this.update({
+            balance,
+            frontier: hash,
+        });
 
         return { hash };
     }
@@ -90,23 +131,7 @@ export default class NanoWallet {
             work: null
         })
 
-        const frontier = data.previous;
-
-        const { work } = await this.rpc.workGenerate(frontier, SEND_DIFFICULTY);
-
-        if (!work) {
-            throw new Error('No work');
-        }
-
-        const isValidWork = validateWork({
-            work,
-            blockHash: frontier,
-            threshold: SEND_DIFFICULTY
-        });
-
-        if (!isValidWork) {
-            throw new Error('Invalid work');
-        }
+        const work = await this.workGenerate(data.previous, SEND_DIFFICULTY);
 
         const processed = await this.rpc.process({
             ...block,
@@ -116,6 +141,11 @@ export default class NanoWallet {
         if (processed.hash !== hash) {
             throw new Error('Block hash mismatch');
         }
+
+        await this.update({
+            balance: '0',
+            frontier: hash,
+        });
 
         return { hash };
     }
