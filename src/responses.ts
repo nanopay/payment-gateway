212822891393672c60939381721f11a3fa567751
anarkrypto <anarkrypto@gmail.com>
2024-03-02 19:00:43 -0300

    ✨ Add NotFoundException

--- src/responses.ts
+++ src/responses.ts
@@ -10,6 +10,18 @@ export async function UnauthorizedException(reason: string) {
     });
 }
 
+export async function NotFoundException() {
+    return new Response(JSON.stringify({
+        reason: 'Not Found'
+    }), {
+        status: 404,
+        statusText: 'Bad Request',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
 export async function BadRequestException(reason: string) {
     return new Response(JSON.stringify({
         reason
