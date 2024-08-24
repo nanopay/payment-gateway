f4db6d9adbfe046c815068da1ad210b8015ecefb
anarkrypto <anarkrypto@gmail.com>
2024-08-24 20:13:04 -0300

    feat: add router

---
+++ src/utils/router.ts
@@ -0,0 +1,71 @@
+import { match } from 'path-to-regexp';
+import { MethodNotAllowedException, NotFoundException } from '../responses';
+
+type Route<Path extends string, Env = unknown, CfHostMetadata = unknown> = {
+	path: Path;
+	method: string;
+	handler: RouteHandler<Path, Env, CfHostMetadata>;
+};
+
+type ExtractRouteParams<T extends string> = T extends `${infer _Start}:${infer Param}/${infer Rest}`
+	? { [K in Param | keyof ExtractRouteParams<`/${Rest}`>]: string }
+	: T extends `${infer _Start}:${infer Param}`
+	? { [K in Param]: string }
+	: {};
+
+type RouteHandler<Path extends string, Env = unknown, CfHostMetadata = unknown> = ({
+	req,
+	params,
+	env,
+	ctx,
+}: {
+	req: Request<CfHostMetadata, IncomingRequestCfProperties<CfHostMetadata>>;
+	params: ExtractRouteParams<Path>;
+	env: Env;
+	ctx: ExecutionContext;
+}) => Response | Promise<Response>;
+
+export class Router<Env = unknown, CfHostMetadata = unknown> {
+	private routes: Route<string, Env, CfHostMetadata>[] = [];
+
+	private addRoute<Path extends string>(path: Path, method: string, handler: RouteHandler<Path, Env>) {
+		this.routes.push({ path, method, handler });
+	}
+
+	public get<Path extends string>(path: Path, handler: RouteHandler<Path, Env>) {
+		this.addRoute(path, 'GET', handler);
+	}
+
+	public post<Path extends string>(path: Path, handler: RouteHandler<Path, Env>) {
+		this.addRoute(path, 'POST', handler);
+	}
+
+	public fetch: ExportedHandlerFetchHandler<Env, CfHostMetadata> = async (req, env, ctx) => {
+		const { pathname } = new URL(req.url);
+
+		let pathExists = false;
+		let params: ExtractRouteParams<typeof pathname> = {};
+
+		const route = this.routes.find((route) => {
+			const result = match(route.path)(pathname);
+			if (result) {
+				pathExists = true;
+				if (route.method === req.method) {
+					params = result.params;
+					return true;
+				}
+			}
+		});
+
+		if (!route) {
+			return pathExists ? MethodNotAllowedException() : NotFoundException();
+		}
+
+		return await route.handler({
+			req,
+			params,
+			env,
+			ctx,
+		});
+	};
+}
